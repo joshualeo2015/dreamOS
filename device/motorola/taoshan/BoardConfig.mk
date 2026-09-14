de43bd698ccfa381ee5fa18d07901eb7aa6e06a7
joshualeo2015 <joshualeofamily@gmail.com>
2026-09-14 10:29:12 +0000

    Fixed file tracking and saved DreamOS data

---
+++ device/motorola/taoshan/BoardConfig.mk
@@ -0,0 +1,7 @@
+# Moto E3 Power Hardware Interface Configuration
+TARGET_BOARD_PLATFORM := mt6735
+TARGET_CPU_ABI := armeabi-v7a
+
+BOARD_USES_MTK_HARDWARE := true
+BOARD_RIL_CLASS := ../../../hardware/ril/
+TARGET_PROVIDES_LIBRIL := true
--- install_dreamos.sh
+++ install_dreamos.sh
@@ -41,3 +41,4 @@ TARGET_CPU_ABI := armeabi-v7a
 BOARD_USES_MTK_HARDWARE := true
 BOARD_RIL_CLASS := ../../../hardware/ril/
 TARGET_PROVIDES_LIBRIL := true
+INNER_EOF
